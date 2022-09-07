c89e75bbb55a962a8bf08e3b21be6d82ff401b3a
HudsonOnHere <wright.richard94@icloud.com>
2022-09-06 22:59:56 -0400

    Initial commit
    Repo created with larger project in mind. Initial, working version of installer tool and accompanying schema file.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Deployment Tools
+
+A (soon to be) collection of scripts for IT Admins in smaller companies. Aimed at automating some commonly performed tasks.
+
+## Work in Progress
+
+More to come on this project, check back later.
+
+### Installer.ps1
+
+This installer script is used to automate multiple software download and installs on a local machine. The idea is that you can save space and time by having an automated download and install for your line of business apps, rather than transferring the same handful of MSI installer files to the target machine with every deployment.
+
+run `Get-Help .\installer.ps1 -full` for all of the details and example usage.
---
+++ installer.ps1
@@ -0,0 +1,94 @@
+<#
+
+.SYNOPSIS
+A script for IT Admins in smaller companies to automate their software deplopyments on a per-machine basis.
+
+.DESCRIPTION
+This installer script is used to automate multiple software download and installs on a local machine. It downloads from the source specified in schema.json and outputs the downloaded files to C:\Windows\Tasks. Arguments are also specified in schema.json, default is "/passive" for silent installs. Change this to "/?" if you are adding your own sources and need to test them.
+
+This installer script can also be used to download and install Lenovo Commercial Vantage, which is particularly useful in deployment of a generalized Windows image. See EXAMPLE for how-to's.
+
+.EXAMPLE
+PS> .\installer.ps1 box
+
+.EXAMPLE
+PS> .\installer.ps1 -Software slack, egnyte
+
+.EXAMPLE
+PS> .\installer.ps1 -Vantage $True
+
+.EXAMPLE
+PS> .\installer.ps1 webex -Vantage $True
+
+.PARAMETER Software
+Specify the software you want to download and install. Valid choices are "box, egnyte, slack, and/or webex". Mutiple values must be separated by commas, see EXAMPLE.
+
+.PARAMETER Vantage
+Optional, default is false. If true, Lenovo Commercial Vantage will be downloaded, unzipped, and installed locally. See EXAMPLE.
+
+.NOTES
+Installer (c) 2022 Rich Wright [HudsonOnHere]
+
+This software is provided "as is" with no warranties or guarantees of any kind.
+See LICENSE for the full terms.
+
+#>
+
+
+#Requires -RunAsAdministrator
+
+Param(
+
+    [Parameter(Position = 0)]
+    [ValidateSet('Box', 'Egnyte', 'Slack', 'Webex')]
+    [string[]]$Software,
+    [bool]$Vantage = $false
+
+)
+
+
+$ScriptPath = $MyInvocation.MyCommand.Path
+$ScriptPathParent = Split-Path $ScriptPath -Parent
+$SchemaPath = $ScriptPathParent + "\schema.json"
+$Schema = Get-Content $SchemaPath -raw | ConvertFrom-Json
+
+
+foreach ($i in $Software) {
+
+    $Source = $Schema.software.$i.source
+    $OutFile = $Schema.software.$i.destination
+    $Arguments = $Schema.software.$i.arguments
+
+
+    Write-Host "Starting a BITS transfer for $i"
+    Start-BitsTransfer -DisplayName $i -Source $Source -Destination $OutFile
+
+    Start-Process $OutFile -ArgumentList $Arguments
+    Wait-Process (Get-Process -Name "msiexec").Id
+    Remove-Item -Path $OutFile -Force -Confirm:$false -ErrorAction SilentlyContinue
+    
+}
+
+
+switch ($Vantage) {
+
+    $true {
+
+        $Source = $Schema.vantage.source
+        $OutFile_Zipped = $Schema.vantage.destination
+        $Outfile_Unzipped = $Schema.vantage.unzipped_package
+        $Installer = $Schema.vantage.installer_path
+
+        Start-BitsTransfer -DisplayName "Lenovo Vantage" -Source $Source -Destination $OutFile_Zipped
+        Expand-Archive -Path $OutFile_Zipped -DestinationPath $Outfile_Unzipped
+
+        $Process = Start-Process $Installer -PassThru
+        # $Process = Start-Process notepad -PassThru
+        Wait-Process $Process.Id
+
+        Remove-Item -Path $OutFile_Zipped, $Outfile_Unzipped -Recurse -Force -Confirm:$false -ErrorAction SilentlyContinue
+
+    }
+}
+
+
